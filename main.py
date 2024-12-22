279702cfc8e94a6e047d3fd53eabac044d29b4ea
darkhan1236 <dzejnulla194@gmail.com>
2024-12-22 11:37:46 +0500

    new commit

---
+++ main.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request, redirect 
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:darkhan@localhost:5433/Shop'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    isActive = db.Column(db.Boolean, default=True)
+    
+    def __repr__(self):
+        return self.title
+
+@app.route('/')
+def index():
+    items = Item.query.order_by(Item.price).all()
+    return render_template('index.html', data=items)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/buy/<int:id>')
+def item_buy(id):
+    return str(id)
+
+
+@app.route('/create', methods=['POST', 'GET'])
+def create():
+    if request.method == "POST":
+        title = request.form['title']
+        price = request.form['price']
+        
+        item = Item(title=title, price=price)
+        
+        try:
+            db.session.add(item)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Получилась ошибка"
+    else:
+        return render_template('create.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
